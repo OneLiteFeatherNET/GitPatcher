df3e43229c0764cecc01a8ab717aceed74d854f6
Ryszard Cichosz-Otawski <56263050+Codestech1@users.noreply.github.com>
2023-06-26 14:07:04 +0200

    Add patch task

--- src/main/kotlin/net/onelitefeather/gitpatcher/tasks/patch/PatchTask.kt
+++ src/main/kotlin/net/onelitefeather/gitpatcher/tasks/patch/PatchTask.kt
@@ -1,15 +1,62 @@
 package net.onelitefeather.gitpatcher.tasks.patch
 
-import net.onelitefeather.gitpatcher.tasks.GitTask
-import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.Internal
 import java.io.File
+import net.onelitefeather.gitpatcher.tasks.SubmoduleTask
 
-abstract class PatchTask : GitTask() {
+abstract class PatchTask : SubmoduleTask() {
 
     abstract val root: File
 
     @get:Internal
     abstract val patchDir: File
 
+    private var cachedRefs: List<String>? = null
+
+    protected fun getPatches(): Array<File> {
+        if (!patchDir.isDirectory) {
+            return emptyArray()
+        }
+
+        return patchDir.listFiles { _, name ->
+            name.endsWith(".patch")
+        }.sortedArray()
+    }
+
+    fun getSubmoduleRoot(): File {
+        return File(root, submodule)
+    }
+
+    fun getGitDir(): File {
+        return File(repo, ".git")
+    }
+
+    fun getRefCache(): File {
+        return File(this.getGitDir(), ".gitpatcher_ref")
+    }
+
+    private fun readCache() {
+        if (cachedRefs == null) {
+            val refCache = this.getRefCache()
+            if (refCache.isFile) {
+                this.cachedRefs = refCache.readLines()
+                    .map { it.trim() }
+                    .filter {
+                        it.isNotEmpty() && !it.startsWith("#")
+                    }
+            } else {
+                this.cachedRefs = emptyList()
+            }
+        }
+    }
+
+    fun getCachedRef(): String? {
+        this.readCache()
+        return cachedRefs?.get(0)
+    }
+
+    fun getCachedSubmodules(): String? {
+        this.readCache()
+        return cachedRefs?.get(1)
+    }
 }
